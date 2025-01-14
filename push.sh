5855c42db157dc24fda182b40774bfd2d4d5f1e7
dekuNukem <dekuNukem@gmail.com>
2025-01-14 23:17:01 +0000

    help scripts

---
+++ push.sh
@@ -0,0 +1,14 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'error: please enter a commit comment'
+    exit 1
+fi
+
+find . -type f -name "*.DS_Store*" -exec rm -f {} \;
+find . -type f -name "*.eggs*" -exec rm -f {} \;
+find . -name "__pycache__" -exec rm -rf {} \;
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+git add --all
+git commit -m "$@"
+git push origin "$current_branch"
