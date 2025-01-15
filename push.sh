c892eecfc648c3aeeeeccdc54409920aa29a5210
dekuNukem <dekuNukem@gmail.com>
2025-01-15 18:31:08 +0000

    unlock on push

--- push.sh
+++ push.sh
@@ -7,6 +7,8 @@ find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -type f -name "*.eggs*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
+bundle config set frozen false
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 git add --all
